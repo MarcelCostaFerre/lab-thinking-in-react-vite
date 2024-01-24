f036029eb817621dfaf5e3a467a2fe6f7086545a
Marcel Costa <marcelcosta@gmail.com>
2024-01-25 00:21:47 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import ProductPage from "./components/ProductsPage";
 
 function App() {
   return (
     <div className="App">
       <h1>React | Thinking in React</h1>
+      <ProductPage />
     </div>
   );
 }
---
+++ src/components/ProductsPage.jsx
@@ -0,0 +1,19 @@
+import jsonData from "../data.json";
+import { useState } from "react";
+import ProductTable from "./ProductTable";
+import SearchBar from "./SearchBar";
+
+function ProductPage() {
+  const [products, setProducts] = useState(jsonData);
+  return (
+    <div>
+      <h1>IronStore</h1>
+      <p>Search</p>
+      <SearchBar setProducts={setProducts} products={products}/>
+      <br/>
+      <ProductTable products={products} />
+    </div>
+  );
+}
+
+export default ProductPage;
